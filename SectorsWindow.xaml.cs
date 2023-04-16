2692ba533339fb5688754e06b8798287de9bc232
RomanoK <romanokeser@gmail.com>
2023-04-16 18:42:59 +0200

    Alpha changing functionalities working in SectorsWindow

---
+++ SectorsWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace MouseTracking
+{
+    public partial class SectorsWindow : Window
+    {
+        private List<System.Drawing.Point> _mouseCoords = new List<System.Drawing.Point>();
+        private List<DrawnRectangle> _drawnRectangles = new List<DrawnRectangle>();
+
+        public SectorsWindow()
+        {
+            InitializeComponent();
+            InitCoords();
+            canvas.Loaded += Canvas_Loaded;
+            
+        }
+
+        private void Canvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            DrawRectangleSectors();
+        }
+
+        private void InitCoords()
+        {
+            //gathers all coords and store them into _mouseCoords List<> 
+            List<System.Drawing.Point> mouseCoords = new List<System.Drawing.Point>();
+            string filePath = "MouseCoords.txt";
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(',');
+                    double x = double.Parse(parts[0]);
+                    double y = double.Parse(parts[1]);
+                    mouseCoords.Add(new System.Drawing.Point((int)x, (int)y));
+                }
+            }
+            _mouseCoords = mouseCoords;
+        }
+
+        private void DrawRectangleSectors()
+        {
+            int rectSize = 20;
+            int maxX = (int)canvas.ActualWidth - rectSize;
+            int maxY = (int)canvas.ActualHeight - rectSize;
+            for (int x = 0; x <= maxX; x += rectSize)
+            {
+                for (int y = 0; y <= maxY; y += rectSize)
+                {
+                    Rectangle rectangle = new Rectangle()
+                    {
+                        Width = rectSize,
+                        Height = rectSize,
+                        Stroke = Brushes.Black,
+                        StrokeThickness = 1
+                    };
+                    Canvas.SetLeft(rectangle, x);
+                    Canvas.SetTop(rectangle, y);
+                    canvas.Children.Add(rectangle);
+                    _drawnRectangles.Add(new DrawnRectangle()
+                    {
+                        posXY = new int[,] { { x, y }, { x + rectSize, y + rectSize } }
+                    });
+                }
+            }
+        }
+
+        private void ApplyAlphaChangesToDrawnRectangles()
+        {
+            //iterate through the list of mouse coordinates
+            //iterate through the lit of drawnRectangles and check how many points of mouse coordinate is inside
+            foreach (System.Drawing.Point mouseCoord in _mouseCoords)
+            {
+                foreach (DrawnRectangle drawnRectangle in _drawnRectangles)
+                {
+                    int[,] posXY = drawnRectangle.posXY!;
+                    int left = posXY[0, 0];
+                    int top = posXY[0, 1];
+                    int right = posXY[1, 0];
+                    int bottom = posXY[1, 1];
+
+                    if (mouseCoord.X >= left && mouseCoord.X <= right &&
+                        mouseCoord.Y >= top && mouseCoord.Y <= bottom)
+                    {
+                        Rectangle rectangle = canvas.Children[_drawnRectangles.IndexOf(drawnRectangle)] as Rectangle;
+                        SolidColorBrush brush = rectangle.Fill as SolidColorBrush;
+                        if (brush == null)
+                        {
+                            brush = new SolidColorBrush(Colors.Blue);
+                            rectangle.Fill = brush;
+                        }
+                        double alpha = brush.Color.A - 10;
+                        if (alpha < 0)
+                        {
+                            alpha = 0;
+                        }
+                        brush.Color = Color.FromArgb((byte)alpha, brush.Color.R, brush.Color.G, brush.Color.B);
+                    }
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ApplyAlphaChangesToDrawnRectangles();
+        }
+    }
+
+    public class DrawnRectangle
+    {
+        public int[,]? posXY;
+    }
+}
--- ThirdWindow.xaml.cs
+++ ThirdWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace MouseTracking
 {
@@ -45,8 +37,6 @@ namespace MouseTracking
                 }
             }
 
-
-
             heatmapBitmap = new WriteableBitmap(ImageWidth, ImageHeight, 96, 96, PixelFormats.Gray8, null);
             heatmapPixels = new byte[ImageWidth * ImageHeight];
 
@@ -61,21 +51,89 @@ namespace MouseTracking
 
         private void UpdateHeatmap(System.Drawing.Point mousePos)
         {
+            //// Convert the mouse position to coordinates within the heatmap image
+            //float x = (float)mousePos.X / ImageWidth;
+            //float y = (float)mousePos.Y / ImageHeight;
+            //int pixelX = (int)(x * ImageWidth);
+            //int pixelY = (int)(y * ImageHeight);
+
+            //// Add a value to the heatmap at the mouse position
+            //if (pixelX >= 0 && pixelX < ImageWidth && pixelY >= 0 && pixelY < ImageHeight)
+            //{
+            //    int colorIntensity = 255; // Scale the density to fit within 0-255
+            //    Color color = Color.FromArgb(255, 0, (byte)(255 - colorIntensity), (byte)colorIntensity); // Map the density value to a color
+            //    byte[] pixelValues = new byte[] { color.B, color.G, color.R, 0 }; // Convert the color to BGR values
+            //    int stride = ImageWidth * 4; // 4 bytes per pixel (BGR0)
+            //    heatmapBitmap.WritePixels(new Int32Rect(pixelX, pixelY, 1, 1), pixelValues, stride, 0); // Set the pixel value
+            //}
+
+            // Define a radius around the mouse position
+            float radius = 0f; // Change this value as needed
+
             // Convert the mouse position to coordinates within the heatmap image
             float x = (float)mousePos.X / ImageWidth;
             float y = (float)mousePos.Y / ImageHeight;
-            int pixelX = (int)(x * ImageWidth);
-            int pixelY = (int)(y * ImageHeight);
+            int centerPixelX = (int)(x * ImageWidth);
+            int centerPixelY = (int)(y * ImageHeight);
 
-            // Add a value to the heatmap at the mouse position
-            if (pixelX >= 0 && pixelX < ImageWidth && pixelY >= 0 && pixelY < ImageHeight)
+            // Define a color gradient to map density values to colors
+            Color[] gradient = new Color[]
+            {   Color.FromArgb(255, 0, 0, 255),
+                Color.FromArgb(255, 0, 255, 0),
+                Color.FromArgb(255, 255, 255, 0),
+                Color.FromArgb(255, 255, 0, 0)
+            };
+            // Iterate over all pixels within the radius around the mouse position
+            for (int pixelX = centerPixelX - (int)radius; pixelX <= centerPixelX + (int)radius; pixelX++)
             {
-                int density = 255; // Scale the density to fit within 0-255
-                Color color = Color.FromArgb(255, 0, (byte)(255 - density), (byte)density); // Map the density value to a color
-                byte[] pixelValues = new byte[] { color.B, color.G, color.R, 0 }; // Convert the color to BGR values
-                int stride = ImageWidth * 4; // 4 bytes per pixel (BGR0)
-                heatmapBitmap.WritePixels(new Int32Rect(pixelX, pixelY, 1, 1), pixelValues, stride, 0); // Set the pixel value
+                for (int pixelY = centerPixelY - (int)radius; pixelY <= centerPixelY + (int)radius; pixelY++)
+                {
+                    // Calculate the distance between the current pixel and the mouse position
+                    float distance = (float)Math.Sqrt(Math.Pow(pixelX - centerPixelX, 2) + Math.Pow(pixelY - centerPixelY, 2));
+
+                    // If the distance is within the radius, update the pixel color
+                    if (distance <= radius && pixelX >= 0 && pixelX < ImageWidth && pixelY >= 0 && pixelY < ImageHeight)
+                    {
+                        // Calculate the color density based on the distance from the center pixel
+                        int maxDensity = 255;
+                        int density = maxDensity - (int)((distance / radius) * maxDensity);
+
+                        // Map the density value to a color using the gradient
+                        Color color;
+                        if (density <= 0)
+                        {
+                            color = gradient[0];
+                        }
+                        else if (density >= maxDensity)
+                        {
+                            color = gradient[gradient.Length - 1];
+                        }
+                        else
+                        {
+                            float t = (float)density / maxDensity;
+                            int index = (int)Math.Floor((gradient.Length - 1) * t);
+                            float fraction = ((gradient.Length - 1) * t) - index;
+                            color = InterpolateColors(gradient[index], gradient[index + 1], fraction);
+                        }
+
+                        // Convert the color to BGR values
+                        byte[] pixelValues = new byte[] { color.B, color.G, color.R, 0 };
+
+                        // Set the pixel value
+                        int stride = ImageWidth * 4; // 4 bytes per pixel (BGR0)
+                        heatmapBitmap.WritePixels(new Int32Rect(pixelX, pixelY, 1, 1), pixelValues, stride, 0);
+                    }
+                }
             }
+        }
+
+        private Color InterpolateColors(Color color1, Color color2, float fraction)
+        {
+            int r = (int)((1 - fraction) * color1.R + fraction * color2.R);
+            int g = (int)((1 - fraction) * color1.G + fraction * color2.G);
+            int b = (int)((1 - fraction) * color1.B + fraction * color2.B);
+            int a = (int)((1 - fraction) * color1.A + fraction * color2.A);
+            return Color.FromArgb((byte)a, (byte)r, (byte)g, (byte)b);
         }
     }
 }
